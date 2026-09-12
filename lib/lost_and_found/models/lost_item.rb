fae8b114fa00e4d68e24c9e534db37f89866dd22
Jianqiu Wang <jianqiuwang1@icloud.com>
2026-09-12 12:47:16 -0500

    Implement LostItem and FoundItem subclasses

--- lib/lost_and_found/models/lost_item.rb
+++ lib/lost_and_found/models/lost_item.rb
@@ -1,1 +1,18 @@
+require_relative "item"
+
 class LostItem < Item
+  attr_reader :date_lost
+
+  def initialize(name:, description:, category:, location:, date_lost:)
+    super(
+      name: name,
+      description: description,
+      category: category,
+      location: location,
+      date: date_lost,
+      status: "Lost"
+    )
+
+    @date_lost = date_lost
+  end
+end
